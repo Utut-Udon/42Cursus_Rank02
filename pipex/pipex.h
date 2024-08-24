a1ec105131b59accba44737315e181412fc7fc6b
hfujita <c07014855@gmail.com>
2024-08-24 18:11:30 +0900

    pipex_start

---
+++ pipex/pipex.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fujitaharuki <fujitaharuki@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 15:26:13 by fujitaharuk       #+#    #+#             */
+/*   Updated: 2024/08/24 17:15:43 by fujitaharuk      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include "libft.h"
+# include <fcntl.h>
+# include <sys/type.h>
+# include <sys/uio.h>
+# include <stdio.h>
+# include <
+
+#endif
